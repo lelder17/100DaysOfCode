c597f6c85ff353c0558f5279af6a20a4a203445b
LaShawnda Elder <lelder@redventures.com>
2021-02-19 23:27:11 -0500

    starting to syle home page

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.onload = function () {
   const enterButton = document.getElementById('js-button');
   enterButton.addEventListener('click', (e) => {
     e.preventDefault();
-    window.location = 'purecss.html';
+    window.location = 'home.html';
   });
 };
